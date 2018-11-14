95cd669aafe6dc4256dbd02c28f50f9ffc928474
mrOlafson <m.c.bernd@hotmail.com>
2018-11-14 15:09:55 +0100

    remade goodbye command, remade help command

--- puchi.py
+++ puchi.py
@@ -29,7 +29,8 @@ embed.add_field(name="-=Commands =-", value="(do not add () in the command)", in
 embed.add_field(name="pu.ht (numbers)", value="outputs the demanded nhentai.net link  ", inline=False)
 embed.add_field(name="pu.ht_rd", value="outputs a random nhenai.net link", inline=False)
 embed.add_field(name="pu.cl (number)", value="deletes (number) messages (may take a moment)", inline=False)
-embed.add_field(name="pu.bulkcl (number)", value="deletes (number) messages at once", inline=True)
+embed.add_field(name="pu.bulkcl (number)", value="deletes (number) messages at once", inline=False)
+embed.add_field(name="pu.invite", value="sends you an invitation link to add the bot", inline=False)
 embed.set_footer(text="broccoli 1.0.0  |  made by SECoY#0181")
 
 
@@ -75,8 +76,15 @@ async def on_message(message):
         await client.send_message(message.author,embed=embed)
 
     elif message.content.startswith("pu.goodbye"):
-        await client.close()
-        os.system("puchi.py")
+        if (message.author.id =='223111553891827722'):
+            await client.close()
+            os.system("puchi.py")
+        elif (message.author.id !='223111553891827722'):
+            y = message.content
+
+    elif message.content.startswith("pu.invite"):
+        await client.send_message(message.author, "https://bit.ly/2B5wfaV")
+
     await client.process_commands(message)
 
 
